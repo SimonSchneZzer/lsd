a93930b2968053add0ace2b06dce04b43e2d6fcd
SimonSchneZzer <simsch2002@gmail.com>
2025-04-10 13:03:33 +0200

    database change

--- prisma/migrations/20250409142225_add_unique_course_id_to_attendance/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[courseId]` on the table `Attendance` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Attendance_courseId_key" ON "Attendance"("courseId");
