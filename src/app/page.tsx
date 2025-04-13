244df546c8407a258106d326c6459f930ff5a97b
SimonSchneZzer <simsch2002@gmail.com>
2025-04-13 12:00:48 +0200

    mobile now has all functions aswell, better naming for configurator in sideboard and better icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <div >
       <main>
+        <h2>Welcome to the <b>Lazy Stundent Dashboard!</b></h2><br />
+        <p>The Lazy Student Dashboard <b>(LSD)</b> helps you monitor your attendance and effort so you only do as much work as absolutely necessary!</p>
+        <br />
+        <p>Please <u><a href="/auth/login">sign in</a> </u> or <u> <a href="/auth/register">register</a> </u> to start making the most of doing the least.</p>
       </main>
       <footer>
       </footer>
