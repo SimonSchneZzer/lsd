cf0a44d39fe41c331c3beb096c53fffa03146f67
SimonSchneZzer <simsch2002@gmail.com>
2025-05-14 15:02:57 +0200

    spinner visible again

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,13 +1,11 @@
 'use client';
 
-import React from 'react';
 import styles from './Spinner.module.css';
-
 
 export default function Spinner() {
   return (
-    <div className="spinner-wrapper" role="status">
-      <div className="loader" />
+    <div className={styles.spinnerWrapper} role="status">
+      <div className={styles.loader} />
     </div>
   );
 }
